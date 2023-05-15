8af39be56349010f51314c875666c32efc476a3e
Saral Bajracharya <rmanbajra@gmail.com>
2023-05-15 18:32:56 +0545

    OOP

---
+++ 1_basic_literals.js
@@ -0,0 +1,13 @@
+// const s = 'hello';
+// const s1 = new String('Hello');
+// console.log(typeof s);
+// console.log(typeof s1);
+// console.log(navigator.appVersion);
+// console.log(window);
+
+const book1 = {
+    title: 'Book One',
+    author: 'Tero Dai',
+    year: '2069'
+};
+console.log(book1);
