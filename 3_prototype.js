3f1348fcc7beb3d420028708393bfc289cb4841e
Saral Bajracharya <rmanbajra@gmail.com>
2023-05-15 18:47:20 +0545

    OOP IN JS

---
+++ 3_prototype.js
@@ -0,0 +1,18 @@
+//Constructors
+
+function Book(title, author, year) {
+    this.title = title;
+    this.author =  author;
+    this.year = year;
+}
+
+//getSummary
+Book.prototype.getSummary = function(){
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+
+}
+
+const book1 = new Book('Book one', 'Tero dai', '2069');
+
+
+console.log(book1.getSummary());
