f9c14755233e5a02b28a184a00ef55e2560688be
Saral Bajracharya <rmanbajra@gmail.com>
2023-05-15 19:02:45 +0545

    OOP IN JS

--- 3_prototype.js
+++ 3_prototype.js
@@ -11,8 +11,20 @@ Book.prototype.getSummary = function(){
     return `${this.title} was written by ${this.author} in ${this.year}`;
 
 }
+//getAge
+Book.prototype.getAge = function(){
+    const years = new Date().getFullYear() -this.year;
+    return `${this.title} is ${years} years old`;
+};
 
+//revise/change year
+Book.prototype.revise = function(newYear) {
+    this.year = newYear;
+    this.revised = true;
+}
 const book1 = new Book('Book one', 'Tero dai', '2069');
 
 
-console.log(book1.getSummary());
+console.log(book1.getAge());
+book1.revise('2070');
+console.log(book1);
