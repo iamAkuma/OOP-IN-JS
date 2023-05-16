66e6cd5a8db965d1ac96b745fffb43e38b1e0816
Saral Bajracharya <rmanbajra@gmail.com>
2023-05-16 18:29:56 +0545

    OOP IN JS

---
+++ 4_inheritance.js
@@ -0,0 +1,31 @@
+function Book(title, author, year) {
+    this.title = title;
+    this.author =  author;
+    this.year = year;
+}
+
+//getSummary
+Book.prototype.getSummary = function(){
+    return `${this.title} was written by
+     ${this.author} in ${this.year}`;
+
+}
+
+//Magazine Constructor
+function Magazine(title, author, year, month) {
+    Book.call(this, title, author, year);
+
+    this.month = month;
+}
+//Inherit Prototype
+
+Magazine.prototype = Object.create(Book.prototype);
+
+//Instantiate Magazine Object
+
+const mag1 = new Magazine('mag one', 'tero dai',
+ '2018', 'jan');;
+
+ //Use magazine constructor
+ Magazine.prototype.constructor = Magazine;
+console.log(mag1);
